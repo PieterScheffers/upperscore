e62fb958bb50acec1e2341b46e49bb1134a8a675
Pieter Scheffers <pieter.scheffers@gmail.com>
2015-10-26 20:55:14 +0100

    Added some functions

--- src/array.php
+++ src/array.php
@@ -26,6 +26,46 @@ function indexArray(array $arr, $prop="id") {
 	return $newArr;
 }
 
+/**
+ * arrayFlatten
+ * @param  array $arr multidimenional array
+ * @return array      flat array
+ */
+
+function arrayFlatten(array $arr) {
+
+    $arr = array_reduce($arr, function($a, $item) {
+        if( is_array($item) ) $item = arrayFlatten($item);
+
+        return array_merge($a, (array)$item);
+    }, []);
+
+    return $arr;
+}
+
+/**
+ * arrayDelete
+ * 
+ * Removes an item from the array and returns its value.
+ *
+ * @param array $arr The input array
+ * @param $key The key pointing to the desired value
+ * @return The value mapped to $key or null if none
+ */
+function arrayDelete(array &$arr, $key) {
+    if (array_key_exists($key, $arr)) {
+        $val = $arr[$key];
+        unset($arr[$key]);
+
+        return $val;
+    }
+
+    return null;
+}
+
+
+
+
 // select multiple key/values from array
 function arraySelect(array $arr, callable $cb) {
 	$newArr = [];
@@ -45,4 +85,37 @@ function arrayDetect(array $arr, callable $cb) {
 		}
 	}
 	return null;
+}
+
+
+/**
+ * arrayType
+ * 
+ * Find whether the array is indexed or an associative array
+ * If it is indexed, find if its sparse or not
+ * 
+ * @param  array   $arr array
+ * @return string       type of array (index, assoc, sparse)
+ */
+function arrayType( array $arr=[] ){
+    $last_key = -1;
+    $type = 'index';
+
+    foreach( $arr as $key => $val ){
+
+        if( !is_int( $key ) || $key < 0 ){
+            return 'assoc';
+        }
+
+        if( $type !== 'sparse' ) {
+
+	        if( $key !== $last_key + 1 ){
+	            $type = 'sparse';
+	        }
+	        $last_key = $key;
+
+	    }
+    }
+
+    return $type;
 }
---
+++ src/string.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace pieterscheffers\upperscore;
+
+
+function randomString($length = 10) {
+	$lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	$allChars = str_split($lowerCase . strtoupper($lowerCase) . "0123456789");
+
+	$string = "";
+	for ($j=0; $j < $length; $j++) { 
+		$string .= $allChars[rand(0, count($allChars)-1)];
+	}
+	return $string;
+}

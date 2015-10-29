958e05e0ea9262ac0a9c878acaae69188ca0da05
Pieter Scheffers <pieter.scheffers@gmail.com>
2015-10-29 19:30:41 +0100

    Added sanitizePath, bytesToHuman and xmlEscape

--- src/bin/path.php
+++ src/bin/path.php
@@ -34,3 +34,78 @@ function pathCombine() {
 
 	return implode("/", $newPaths);
 }
+
+
+/**
+ * sanitizePath
+ *
+ * Sanitizes a path, so it is a valid path in Windows & Linux
+ * 
+ * @param  string  $path         string of a path
+ * @param  string  $replacement  character to use for replacement of bad characters
+ * @return string                sanitized path
+ */
+function sanitizePath($path, $replacement = "_") {
+	if($path === '') return $path;
+
+	// https://msdn.microsoft.com/en-us/library/aa365247
+	
+	$forbidden = array(
+		"\0", // NULL
+		"<",
+		">",
+		":",
+		"\"",
+		"/",
+		"\\",
+		"|",
+		"?",
+		"*"
+	);
+
+	$path = str_replace($forbidden, $replacement, $path); // replace forbidden characters
+
+	$forbidden_filenames = array(
+		"CON",
+		"PRN",
+		"AUX",
+		"NUL",
+		"COM1",
+		"COM2",
+		"COM3",
+		"COM4",
+		"COM5",
+		"COM6",
+		"COM7",
+		"COM8",
+		"COM9",
+		"LPT1",
+		"LPT2",
+		"LPT3",
+		"LPT4",
+		"LPT5",
+		"LPT6",
+		"LPT7",
+		"LPT8",
+		"LPT9"
+	);
+
+	if( in_array(strtoupper(basename($path)), $forbidden_filenames) ) {
+		$dirs = explode('/', $path);
+		$lastdir = array_pop($dirs);
+		$fileparts = explode('.', $lastdir);
+		$base = array_shift($fileparts);
+
+		$base = $base . randomString(20); // add random string to basename
+
+		array_unshift($fileparts, $base);
+		$lastdir = implode('.', $fileparts);
+		array_push($dirs, $lastdir);
+		$path = implode('/', $dirs);
+	}
+
+	$path = rtrim($path, '.');  // file not ending with a period
+	$path = trim($path); 		// file not ending with a space
+
+	return $path;
+}
--- src/bin/string.php
+++ src/bin/string.php
@@ -13,3 +13,81 @@ function randomString($length = 10) {
 	}
 	return $string;
 }
+
+
+/**
+ * bytesToHuman
+ *
+ * Takes a int of bytes and turns it into a human readable string
+ * 
+ * @param  int     $bytes   Number of bytes
+ * @param  string  $system  The system to use (binary or si)
+ * @return string           Human readable string
+ */
+function bytesToHuman($bytes, $system='binary') {
+	$binary = array(
+		'B',
+		'KiB',
+		'MiB',
+		'GiB',
+		'TiB',
+		'PiB',
+		'EiB',
+		'ZiB',
+		'YiB'
+	);
+
+	$si = array(
+		'B',
+		'kB',
+		'MB',
+		'GB',
+		'TB',
+		'PB',
+		'EB',
+		'ZB',
+		'YB'
+	);
+
+	$suffixes   = ( ($system !== 'si') ? $binary : $si);
+	$multiplier = ( ($system !== 'si') ? 1024 : 1000);
+
+	foreach ($suffixes as $m) {
+		if($bytes >= $multiplier) {
+			$bytes /= $multiplier;
+		} else {
+			$bytes = round($bytes, 3) . ' ' . $m;
+			break;
+		}
+	}
+
+	return $bytes;
+}
+
+
+/**
+ * xmlEscape
+ *
+ * Sanitizes a string for use in a xml
+ * 
+ * @param  string  $str  dirty string
+ * @return string        sanitized string
+ */
+function xmlEscape($str) {
+	$forbidden = [
+		'"',
+		"&",
+		"'",
+		"<",
+		">"
+	];
+	$replaceWith = [
+		"&quot;",
+		"&amp;",
+		"&apos;",
+		"&lt;",
+		"&gt;"
+	];
+
+	return str_replace($forbidden, $replaceWith, $str);
+}
